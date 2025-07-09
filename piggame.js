9fa23364631e7c42ffa2736f1dae9bec08c51c4f
David Lazic <lazic.ddavid@gmail.com>
2025-07-09 19:36:15 +0200

    added activePlayer

--- piggame.js
+++ piggame.js
@@ -1,9 +1,9 @@
-const totalScore1 = 0;
-const totalScore2 = 0;
-currentScore1 = 0;
-currentScore2 = 0;
-activePlayer = "player1";
+// const totalScore1 = 0;
+// const totalScore2 = 0;
+// currentScore1 = 0;
+// currentScore2 = 0;
+// activePlayer = "player1";
 
-const maxTotalScore = 20;
+// const maxTotalScore = 20;
 
-activePlayer = player1;
+// activePlayer = player1;
